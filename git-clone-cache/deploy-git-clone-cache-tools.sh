db437c9385d85928335384ac464ca14ba8cf9942
Doug Ilijev <doug.ilijev@gmail.com>
2026-02-03 23:49:41 -0800

    Update deploy script

--- git-clone-cache/deploy-git-clone-cache-tools.sh
+++ git-clone-cache/deploy-git-clone-cache-tools.sh
@@ -1,3 +1,4 @@
-cp ./git-clone-cache.sh ~/bin/git
+cp ./git-clone-cache.sh ~/bin/git-clone-cache
+cp ./git-clone-cache-alias.sh ~/bin/git-clone-cache-alias
 cp ./populate-git-clone-cache.sh ~/bin/populate-git-clone-cache
 cp ./find-and-populate-git-clone-cache.sh ~/bin/find-and-populate-git-clone-cache
